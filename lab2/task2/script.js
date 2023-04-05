47fd2f392ea6b00965e33a5861fdf893cdda7318
Sebastian Piaskowy <sebi77styp@gmail.com>
2023-04-05 23:09:39 +0200

    lab3 updated

--- lab2/task2/script.js
+++ lab2/task2/script.js
@@ -1,179 +1,18 @@
-// const DBDeleteRequest = window.indexedDB.deleteDatabase("VehicleDatabase");
+import { generateRandomData } from './randomDB.js';
 
-const mockup = {
-    1: {
-        model_id: 1,
-        type: "rower wyścigowy",
-        brand: "BOTTECCHIA",
-        img_url: "https://a.allegroimg.com/s1024/0c8ff0/ffd228304675a5472e03be4fdf3b",
-        rent_by: null
-    },
-    2: {
-        model_id: 2,
-        type: "rower wyścigowy",
-        brand: "BOTTECCHIA",
-        img_url: "https://a.allegroimg.com/s1024/0c8ff0/ffd228304675a5472e03be4fdf3b",
-        rent_by: null
-    },
-    3: {
-        model_id: 1,
-        type: "rower górski",
-        brand: "Kross Hexagon",
-        img_url: "https://www.greenbike.pl/images/hexagon_black_white_graphite_glossy22.jpg",
-        rent_by: "Jan Kowalski"
-    },
-    4: {
-        model_id: 2,
-        type: "rower górski",
-        brand: "Kross Hexagon",
-        img_url: "https://www.greenbike.pl/images/hexagon_black_white_graphite_glossy22.jpg",
-        rent_by: "Justyna Kowalska"
-    },
-    5: {
-        model_id: 1,
-        type: "hulajnoga",
-        brand: "TECHLIFE",
-        img_url: "https://e-hulajnoga.pl/public/assets/techlife/x8/1.jpg",
-        rent_by: null
-    },
-}
-
-function rentVehicle(event) {
-    event.preventDefault(); 
-
-    const input = document.getElementById('rent-vehicle-input');
-    const form_arguments = input.value.split(', ');
-    if (form_arguments.length != 2) {
-        console.log("%c Niepoprawna ilość argumentów. Oczekiwano 2.\nPrzykład argumentów: '23, Jan Kowalski'\n", "color: red");
-        return
-    }
-
-    const vehicle_id = Number.parseInt(form_arguments[0]);
-    const borrower_name = form_arguments[1];
-
-    // if (!mockup.hasOwnProperty(vehicle_id)) {
-    //     console.log("%c Nie ma takiego pojazdu w bazie danych\n", 'color: red');
-    //     return
-    // }
-    // const vehicle = mockup[vehicle_id];
-
-    // if (vehicle.rent_by == null) {
-    //     mockup[vehicle_id].rent_by = borrower_name;
-
-    //     console.log(`%c ${vehicle.type} marki ${vehicle.brand} o numerze ${vehicle_id} został/a wypożyczony/a na nazwisko ${borrower_name.split(" ")[1]}\n`, 'color: green');
-    // } else {
-    //     console.log("%c Pojazd ten już został przez kogoś wypożyczony\n", 'color: red');
-    // }
-
-    const db = request.result;
-    const transaction = db.transaction("vehicles", "readwrite");
-
-    const store = transaction.objectStore("vehicles");
-    const idQuery = store.get(vehicle_id);
-
-    idQuery.onsuccess = function () {
-        const vehicle = idQuery.result;
-        
-        if (vehicle.rent_by == null) {
-            
-            vehicle.rent_by = borrower_name;
-
-            const updateVehicleRequest = store.update(vehicle);
-
-            updateVehicleRequest.onsuccess = () => {
-                console.log(`%c ${vehicle.type} marki ${vehicle.brand} o numerze ${vehicle_id} został/a wypożyczony/a na nazwisko ${borrower_name.split(" ")[1]}\n`, 'color: green');
-            }
-            db.close();
-
-        } else {
-            console.log("%c Pojazd ten już został przez kogoś wypożyczony\n", 'color: red');
-            db.close();
-            return
-        }
-    };
-
-    idQuery.onerror = function () {
-        console.log("%c Nie ma takiego pojazdu w bazie danych\n", 'color: red');
-        db.close();
-        return
-    }
-    
-    transaction.oncomplete = function () {
-        console.log("Zamykanie transakcji ");
-        db.close();
-    };
-}
-
-
-function returnVehicle(event) {
-    event.preventDefault(); 
-    
-    const input = document.getElementById('return-vehicle-input');
-    const form_arguments = input.value.split(', ');
-    if (form_arguments.length != 2) {
-        console.log("%c Niepoprawna ilość argumentów. Oczekiwano 2.\nPrzykład argumentów: '11, Janusz Tracz'\n", "color: orange");
-        return
-    }
-    
-    const vehicle_id = Number.parseInt(form_arguments[0]);
-    const borrower_name = form_arguments[1];
-    
-    
-    if (!mockup.hasOwnProperty(vehicle_id)) {
-        console.log("%c Nie ma takiego pojazdu w bazie danych\n", "color: red");
-        return
-    }
-    const vehicle = mockup[vehicle_id];
-    
-    if (vehicle.rent_by != null && borrower_name.localeCompare(vehicle.rent_by) == 0) {
-        vehicle.rent_by = null;
-
-        console.log(`%c Pan/Pani ${borrower_name} zwraca ${vehicle.type} marki ${vehicle.brand} o numerze ${vehicle_id}\n`, 'color: green');
-    }
-    else {
-        console.log(`%c Pan/Pani ${borrower_name} nie wypożyczył/a tego pojazdu\n`, 'color: red');
-    }
-
-}
-
-
-function infoVehicle(event) {
-    event.preventDefault(); 
-
-    // for (const [key, value] of Object.entries(mockup)) {
-    //     console.log({key}, value);
-    // };
-    const db = request.result;
-    const transaction = db.transaction("vehicles", "readwrite");
-
-    const store = transaction.objectStore("vehicles");
-    let viewAllRequest = store.getAll();
-    viewAllRequest.onsuccess = (event) => {
-        console.log("Wszystkie pojazdy:");
-        let vehicles = viewAllRequest.result;
-        console.log(vehicles);
-        for (let i = 0; i < vehicles.length; i++) {
-            console.log("ID pojazdu: " + vehicles[i].id);
-            console.log("ID modelu: " + vehicles[i].model_id);
-            console.log("Rodzaj pojazdu: " + vehicles[i].type);
-            console.log("Marka: " + vehicles[i].brand);
-            console.log("URL zdjęcia: " + vehicles[i].img_url);
-            console.log("Wypożyczający: " + vehicles[i].rent_by);
-            console.log("-----------------------");
-        }
-    }
-}
+const DBDeleteRequest = window.indexedDB.deleteDatabase("VehicleDatabase");
 
 function dbQuery() {
     const db = request.result;
     const transaction = db.transaction("vehicles", "readwrite");
 
     const store = transaction.objectStore("vehicles");
-    const model_idIndex = store.index("model_id");
+    const model_idIndex = store.index("model");
     const typeIndex = store.index("type");
     const brandIndex = store.index("brand");
     const img_urlIndex = store.index("img_url");
     const rent_byIndex = store.index("rent_by");
+
     const idQuery = store.get(2);
     const modelIDQuery = model_idIndex.getAll([2]);
 
@@ -190,6 +29,244 @@ function dbQuery() {
     };
 }
 
+function addToDB(item) {
+    const db = request.result;
+    const transaction = db.transaction("vehicles", "readwrite");
+    const objectStore = transaction.objectStore("vehicles");
+    
+    const addRequest = objectStore.put(item);
+
+    addRequest.onsuccess = () => {
+        // console.log(item, "Udało sie dodac obiekt do DB");
+        console.log("Udało sie dodac obiekt do DB");
+    }
+
+    // transaction.oncomplete = function () {
+    //     console.log("Zamykanie transakcji ");
+    //     db.close();
+    // };
+}
+
+function rentVehicle(event) {
+    event.preventDefault(); 
+
+    const input = document.getElementById('rent-vehicle-input');
+    const form_arguments = input.value.split(', ');
+    if (form_arguments.length != 2) {
+        console.log("%c Niepoprawna ilość argumentów. Oczekiwano 2.\nPrzykład argumentów: '23, Jan Kowalski'\n", "color: orange; font-size: 16px; font-weight: bold");
+        return
+    }
+    const vehicle_id = Number.parseInt(form_arguments[0]);
+    const borrower_name = form_arguments[1];
+
+
+    const db = request.result;
+    const transaction = db.transaction("vehicles", "readwrite");
+
+    const objectStore = transaction.objectStore("vehicles");
+
+    const vehicleToUpdate = objectStore.get(vehicle_id);
+
+    vehicleToUpdate.onsuccess = () => {
+        const updateData = vehicleToUpdate.result;
+        // console.log("GET result: ", updateData);
+        if (updateData != undefined) {
+            if (updateData.rent_by == null) {
+                updateData.rent_by = borrower_name;
+                const updateRequest = objectStore.put(updateData);
+                updateRequest.onsuccess = () => {
+                    console.log(`%c ${updateData.type} marki ${updateData.brand} o numerze ID ${vehicle_id} został/a wypożyczony/a na nazwisko ${borrower_name.split(" ")[1]}\n`, 'color: green; font-size: 16px; font-weight: bold');
+                };
+
+            } else {
+                console.log("%c Pojazd ten już został przez kogoś wypożyczony\n", 'color: red; font-size: 16px; font-weight: bold');
+            }
+        } else {
+            console.log("%c Pojazd o takim numerze ID nie istnieje\n", 'color: red; font-size: 16px; font-weight: bold');
+        }
+    };
+
+    vehicleToUpdate.onerror = () => {
+        console.log("Błąd operacji GET");
+    };
+}
+
+function returnVehicle(event) {
+    event.preventDefault(); 
+    
+    const input = document.getElementById('return-vehicle-input');
+    const form_arguments = input.value.split(', ');
+    if (form_arguments.length != 2) {
+        console.log("%c Niepoprawna ilość argumentów. Oczekiwano 2.\nPrzykład argumentów: '11, Janusz Tracz'\n", "color: orange; font-size: 16px; font-weight: bold");
+        return
+    }
+    
+    const vehicle_id = Number.parseInt(form_arguments[0]);
+    const borrower_name = form_arguments[1];
+    
+    const db = request.result;
+    const transaction = db.transaction("vehicles", "readwrite");
+
+    const objectStore = transaction.objectStore("vehicles");
+
+    const vehicleToUpdate = objectStore.get(vehicle_id);
+
+    vehicleToUpdate.onsuccess = () => {
+        const updateData = vehicleToUpdate.result;
+        // console.log("GET result: ", updateData);
+        if (updateData != undefined) {
+            if (borrower_name.localeCompare(updateData.rent_by) == 0) {
+                updateData.rent_by = null;
+                const updateRequest = objectStore.put(updateData);
+                updateRequest.onsuccess = () => {
+                    console.log(`%c Pan/Pani ${borrower_name} zwraca ${updateData.type} marki ${updateData.brand} o numerze ID: ${vehicle_id}\n`, 'color: green; font-size: 16px; font-weight: bold');
+                };
+            } else {
+                console.log(`%c Pan/Pani ${borrower_name} nie wypożyczył/a tego pojazdu\n`, "color: red; font-size: 16px; font-weight: bold");
+            }
+        } else {
+            console.log("%c Pojazd o takim numerze ID nie istnieje\n", 'color: red; font-size: 16px; font-weight: bold');
+        }
+    };
+
+    vehicleToUpdate.onerror = () => {
+        console.log("Błąd operacji GET");
+    };
+
+}
+
+function infoVehicle(event) {
+    event.preventDefault(); 
+
+    const db = request.result;
+    const transaction = db.transaction("vehicles", "readwrite");
+
+    const objectStore = transaction.objectStore("vehicles");
+    const viewAllRequest = objectStore.getAll();
+    viewAllRequest.onsuccess = () => {
+        console.log(viewAllRequest.result);
+        const data = {};
+        for (const vehicle of Object.values(viewAllRequest.result)) {
+            if (vehicle.rent_by != null) break;
+
+            data[vehicle.name] = data[vehicle.name] ? data[vehicle.name] + 1 : 1;
+        }
+        plotChart(data);
+    }
+    // viewAllRequest.onsuccess = (event) => {
+    //     let vehicles = viewAllRequest.result;
+    //     console.log("Wszystkie pojazdy:");
+    //     console.log(vehicles);
+        
+    //     plotChart(objectStore)
+
+    //     // for (let i = 0; i < vehicles.length; i++) {
+    //     //     console.log("ID pojazdu: " + vehicles[i].id);
+    //     //     // console.log("Rodzaj pojazdu: " + vehicles[i].type);
+    //     //     // console.log("Marka: " + vehicles[i].brand);
+    //     //     // console.log("ID modelu: " + vehicles[i].model_id);
+    //     //     // console.log("Nazwa produktu: " + vehicles[i].name);
+    //     //     // console.log("URL zdjęcia: " + vehicles[i].img_url);
+    //     //     console.log("Wypożyczający: " + vehicles[i].rent_by);
+    //     //     console.log("-----------------------");
+    //     // }
+    // }
+}
+
+let chartShowed = false;
+let myChart = null;
+
+function plotChart(vehicleData) {
+        if (chartShowed) {
+            chartShowed = false;
+            myChart.destroy();
+            return
+        }
+        
+        chartShowed = true; 
+        const ctx = document.getElementById('myChart');      
+
+        // const data = {
+        //     labels: Object.keys(vehicleData),
+        //     datasets: [{
+        //         label: 'Ilość sztuk',
+        //         data: Object.values(vehicleData),
+        //         // backgroundColor: [
+        //         //     'rgb(0, 255, 0)',
+        //         //     'rgb(0, 0, 255)',
+        //         //     'rgb(255, 0, 0)'
+        //         // ],
+        //         hoverOffset: 4
+        //     }]
+        // };
+
+        // const config = {
+        //     type: 'pie',
+        //     data: data,
+        // };
+
+        const data = {
+        labels: Object.keys(vehicleData),
+        datasets: [{
+                label: 'Ilość sztuk',
+                data: Object.values(vehicleData),
+                backgroundColor: ["#00ff00aa"],
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: function(val, index) {
+                                // Hide every tick label with non integer value
+                                return val % 1 === 0 ? this.getLabelForValue(val) : '';
+                            },
+                        },
+                    }
+                },
+                maxBarThickness: 80,
+            },
+        };
+        
+        myChart = new Chart(ctx, config);
+        ctx.style.width = '500px';
+        ctx.style.height = '500px';
+}
+
+function returnAllVehicles(event) {
+    event.preventDefault();
+
+    const db = request.result;
+
+    const transaction = db.transaction(["vehicles"], "readwrite");
+    const objectStore = transaction.objectStore("vehicles");
+
+    objectStore.openCursor().onsuccess = (event) => {
+    const cursor = event.target.result;
+    if (cursor) {
+        const updateData = cursor.value;
+
+        updateData.rent_by = null;
+
+        const request = cursor.update(updateData);
+        request.onsuccess = () => {
+            console.log(`Pojazd ID ${cursor.value.id} zwrócony`);
+        };
+        
+        cursor.continue();
+    } else {
+        console.log("Koniec aktualizacji.");
+    }
+  };
+
+
+}
 
 const indexedDB =
   window.indexedDB ||
@@ -210,28 +287,39 @@ request.onerror = function(event) {
 
 request.onupgradeneeded = function () {
     const db = request.result;
-    const store = db.createObjectStore("vehicles", { keyPath: "id" });
+    const objectStore = db.createObjectStore("vehicles", { keyPath: "id", autoIncrement: true });
     
-    store.createIndex("model_id", ["model_id"], { unique: false});
-    store.createIndex("type", ["type"], { unique: false});
-    store.createIndex("brand", ["brand"], { unique: false});
-    store.createIndex("img_url", ["img_url"], { unique: false});
-    store.createIndex("rent_by", ["rent_by"], { unique: false});
+    objectStore.createIndex("type", ["type"], { unique: false});
+    objectStore.createIndex("brand", ["brand"], { unique: false});
+    objectStore.createIndex("model", ["model"], { unique: false});
+    objectStore.createIndex("name", ["name"], { unique: false});
+    objectStore.createIndex("price", ["price"], { unique: false});
+    objectStore.createIndex("img_url", ["img_url"], { unique: false});
+    objectStore.createIndex("rent_by", ["rent_by"], { unique: false});
 };
 
 request.onsuccess = function () {
-    console.log("Baza danych została otwarta pomyślnie ");
+    console.log("Baza danych została otwarta pomyślnie ");   
 
-    const db = request.result;
-
+    const population = generateRandomData(50);
+    for(let vehicle of Object.values(population)) addToDB(vehicle);
+    // fetch('population.json')
+    //     .then(response => response.json()) 
+    //     .then(data => {
+    //         for(let vehicle of Object.values(data)) addToDB(vehicle);
+    //     })
+    //     .catch(error => console.error(error)); 
 };
 
 
 const rent_form = document.getElementById("rent-vehicle");
 const return_form = document.getElementById("return-vehicle");
 const info_form = document.getElementById("info-vehicle");
+const return_all_form = document.getElementById("return-all-vehicles");
 rent_form.addEventListener('submit', rentVehicle);
 return_form.addEventListener('submit', returnVehicle);
 info_form.addEventListener('submit', infoVehicle);
+return_all_form.addEventListener('submit', returnAllVehicles)
 
 
+        
